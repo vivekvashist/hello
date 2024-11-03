7faeaff2937df7a166e4848d3624e1644f88606d
Vivek <vivekvashist@gmail.com>
2024-11-03 18:43:52 +1030

    Strings, bytes, runes and characters in Go
    https://go.dev/blog/strings

--- hello.go
+++ hello.go
@@ -1,94 +1,65 @@
-package main // package declaration
-
-// single line comment
-
-/* multi
-line
-comment
-*/
+package main
 
 import (
-	"fmt" // std lib package
+	"fmt"
+	"unicode/utf8"
 )
 
 func main() {
-	fmt.Printf("%s\n", "lets go")
-	s := "hello"
-	fmt.Printf("%s\n", s)
-	fmt.Printf("%T\n", s)
-	b := s[0]
-	fmt.Printf("%v\n", b)
-	fmt.Printf("%T\n", b)
-	fmt.Printf("%d\n", len(s))
-	s1 := "😃 💁"
-	fmt.Printf("%s\n", s1)
-	s2 := s1[1:]
-	fmt.Printf("%s\n", s2)
-	s3 := s1[2:]
-	fmt.Printf("%s\n", s3)
-	fmt.Printf("%s, %s\n", s2, s3)
-	fmt.Printf("%d\n", len(s1))
-
-	var r rune = 1000 // int32 (4 bytes)
-	fmt.Printf("%d\n", r)
-	fmt.Printf("%T\n", r)
-
-	var values [3]int
-	values[0] = 2
-	values[1] = 4
-	values[2] = 4
-
-	fmt.Printf("%v\n", values)
-	fmt.Printf("%T\n", values)
-	fmt.Printf("%d\n", len(values))
-	fmt.Printf("%d\n", cap(values))
-
-	var routers [3]string
-	routers[0] = "cor1.pop1"
-	routers[1] = "cor2.pop1"
-	routers[2] = "cor3.pop1"
-
-	fmt.Printf("%T\n", routers)
-	fmt.Printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
-	router := "bdr1.syd1"
-	firstLetter := router[0]
-	fmt.Printf("%b\n", firstLetter) // prints uint8=98
-	fmt.Printf("%d\n", firstLetter) // prints 98 i.e ascii "b"
-	fmt.Printf("%s\n", string(firstLetter))
-
-	fmt.Printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
-	// string is a sequence of bytes
-	hostName := "sw1.pop1"
-	// fmt.Printf("%s\n", hostName)
-	// runehostName := []rune(hostName)
-	// fmt.Printf("%d\n", runehostName) // prints [115 119 49 46 112 111 112 49]
-	// bytehostName := []byte(hostName)
-	// fmt.Printf("%s\n", bytehostName) // prints sw1.pop1
-	// fmt.Printf("%T\n", hostName)
-	// fmt.Printf("%T\n", runehostName)
-	// r1 := rune(1111)
-	// fmt.Printf("%T\n", r1)
-	// fmt.Printf("%d\n", r1)
-	fmt.Printf("%s\n", hostName)    // prints sw1.pop1
-	fmt.Printf("%T\n", hostName)    // type string
-	fmt.Printf("%d\n", hostName[0]) // 115
-	fmt.Printf("%T\n", hostName[0]) // unint8
-	for pos, char := range hostName {
-		fmt.Printf("%d -> %d\n", pos, rune(char))
-		fmt.Printf("%d -> %s\n", pos, string(char))
-		fmt.Printf("%d -> %d\n", pos, byte(char))
+	// code from https://go.dev/blog/strings
+	const sample = "\xbd\xb2\x3d\xbc\x20\xe2\x8c\x98"
+	fmt.Println("Println:")
+	fmt.Println(sample)
+	fmt.Printf("\n")
+	fmt.Println("Byte loop:")
+	for i := 0; i < len(sample); i++ {
+		fmt.Printf("%x ", sample[1])
 	}
-	// prints
-	// 0-> 115
-	// 1 -> 119
-	// 2 -> 49
-	// 3 -> 46
-	// 4 -> 112
-	// 5 -> 111
-	// 6 -> 112
-	// 7 -> 49
+	fmt.Printf("\n")
+	fmt.Printf("\n")
+	fmt.Println("Printf with %x:")
+	fmt.Printf("%x\n", sample)
+	fmt.Printf("\n")
+	fmt.Println("Printf with % x:")
+	fmt.Printf("% x\n", sample)
+	fmt.Printf("\n")
+	fmt.Println("Printf with %q:")
+	fmt.Printf("%q\n", sample)
+	fmt.Printf("\n")
+	fmt.Println("Printf with %+q:")
+	fmt.Printf("%+q\n", sample)
+	fmt.Printf("\n")
 
-	fmt.Printf("%s\n", hostName) // prints sw1.pop1
-	fmt.Printf("%d\n", []rune(hostName))
-	fmt.Printf("%d\n", []byte(hostName))
+	const placeOfInterest = `⌘`
+	fmt.Printf("plain string: ")
+	fmt.Printf("%s", placeOfInterest)
+	fmt.Printf("\n")
+
+	fmt.Printf("quoted string: ")
+	fmt.Printf("%+q", placeOfInterest)
+	fmt.Printf("\n")
+
+	fmt.Printf("Hex bytes: ")
+	for i := 0; i < len(placeOfInterest); i++ {
+		fmt.Printf("%x ", placeOfInterest[i])
+	}
+	fmt.Printf("\n")
+
+	const nihongo = "日本語"
+	for index, runeValue := range nihongo {
+		fmt.Printf("%#U starts at byte position %d\n", runeValue, index)
+	}
+
+	for i := 0; i < len(nihongo); i++ {
+		fmt.Printf("% x", nihongo[i])
+	}
+
+	fmt.Printf("\n")
+	fmt.Printf("\n")
+
+	for i, w := 0, 0; i < len(nihongo); i += w {
+		runeValue, width := utf8.DecodeRuneInString(nihongo[i:])
+		fmt.Printf("%#U starts at byte postion %d\n", runeValue, i)
+		w = width
+	}
 }
