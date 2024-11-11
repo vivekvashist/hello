1bea17c278128e66b01b96ec83eb896605dd296a
Vivek <vivekvashist@gmail.com>
2024-11-11 20:13:39 +1030

    remove file.go

--- file.go
+++ file.go
@@ -7,7 +7,8 @@ import (
 	"os"
 )
 
-func main() {
+// func main() {
+	// https://stackoverflow.com/questions/8757389/reading-a-file-line-by-line-in-go/16615559#16615559
 	file, err := os.Open("README.md")
 	if err != nil {
 		log.Fatal(err)
--- hello.go
+++ hello.go
@@ -1,65 +1,26 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"unicode/utf8"
+	"log"
+	"os"
 )
 
 func main() {
-	// code from https://go.dev/blog/strings
-	const sample = "\xbd\xb2\x3d\xbc\x20\xe2\x8c\x98"
-	fmt.Println("Println:")
-	fmt.Println(sample)
-	fmt.Printf("\n")
-	fmt.Println("Byte loop:")
-	for i := 0; i < len(sample); i++ {
-		fmt.Printf("%x ", sample[1])
+	// https://stackoverflow.com/questions/8757389/reading-a-file-line-by-line-in-go/16615559#16615559
+	file, err := os.Open("README.md")
+	if err != nil {
+		log.Fatal(err)
 	}
-	fmt.Printf("\n")
-	fmt.Printf("\n")
-	fmt.Println("Printf with %x:")
-	fmt.Printf("%x\n", sample)
-	fmt.Printf("\n")
-	fmt.Println("Printf with % x:")
-	fmt.Printf("% x\n", sample)
-	fmt.Printf("\n")
-	fmt.Println("Printf with %q:")
-	fmt.Printf("%q\n", sample)
-	fmt.Printf("\n")
-	fmt.Println("Printf with %+q:")
-	fmt.Printf("%+q\n", sample)
-	fmt.Printf("\n")
+	defer file.Close()
 
-	const placeOfInterest = `⌘`
-	fmt.Printf("plain string: ")
-	fmt.Printf("%s", placeOfInterest)
-	fmt.Printf("\n")
-
-	fmt.Printf("quoted string: ")
-	fmt.Printf("%+q", placeOfInterest)
-	fmt.Printf("\n")
-
-	fmt.Printf("Hex bytes: ")
-	for i := 0; i < len(placeOfInterest); i++ {
-		fmt.Printf("%x ", placeOfInterest[i])
-	}
-	fmt.Printf("\n")
-
-	const nihongo = "日本語"
-	for index, runeValue := range nihongo {
-		fmt.Printf("%#U starts at byte position %d\n", runeValue, index)
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Printf("%s\n", scanner.Text())
 	}
 
-	for i := 0; i < len(nihongo); i++ {
-		fmt.Printf("% x", nihongo[i])
-	}
-
-	fmt.Printf("\n")
-	fmt.Printf("\n")
-
-	for i, w := 0, 0; i < len(nihongo); i += w {
-		runeValue, width := utf8.DecodeRuneInString(nihongo[i:])
-		fmt.Printf("%#U starts at byte postion %d\n", runeValue, i)
-		w = width
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
 	}
 }
